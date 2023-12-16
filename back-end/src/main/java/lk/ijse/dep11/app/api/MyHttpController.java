8fc4a880f0754ca49cf3f91a6ab49d4caf74762c
Danidu Bogoda <danidubogoda95@gmail.com>
2023-12-16 22:36:26 +0530

    Initial commit

---
+++ back-end/src/main/java/lk/ijse/dep11/app/api/MyHttpController.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11.app.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tasks")
+public class MyHttpController {
+}
