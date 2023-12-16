96404370e28096180cd87e7a734b10da933cd57b
Danidu Bogoda <danidubogoda95@gmail.com>
2023-12-16 22:43:18 +0530

    Setup Database

--- back-end/src/main/java/lk/ijse/dep11/app/api/MyHttpController.java
+++ back-end/src/main/java/lk/ijse/dep11/app/api/MyHttpController.java
@@ -6,4 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/tasks")
 public class MyHttpController {
+
+    public void addTask(){
+        System.out.println(" addTask()");
+    }
+
+
+    public void updateTask(){
+        System.out.println("updateTask()");
+    }
+
+
+    public void deleteTask(){
+        System.out.println("deleteTask()");
+    }
+
+
+    public void getAllTask(){
+        System.out.println("getAllTask()");
+    }
+
 }
