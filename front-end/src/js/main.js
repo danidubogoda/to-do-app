ff96ece67475a3b59076b946247b6a8a6275486f
Danidu Bogoda <danidubogoda95@gmail.com>
2024-01-18 13:28:16 +0530

    Finished DELETE and PATCH Methods

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -115,3 +115,50 @@ btnAddElm.addEventListener('click', ()=>{
         alert("Something went wrong, try again later");
     });
 });
+
+
+taskContainerElm.addEventListener('click', (e)=>{
+    if (e.target?.classList.contains('delete')){
+
+        const taskId = e.target.closest('li').id.substring(5);
+        console.log("DELETE ", taskId);
+
+        fetch(`${API_URL}/tasks/${taskId}`, {method: 'DELETE'})
+        .then(res => {
+            if (res.ok){
+                e.target.closest("li").remove();
+            }else{
+                alert("Failed to delete the task");
+            }
+        }).catch(err => {
+            alert("Something went wrong, try again later");
+        });  
+
+    } else if (e.target?.tagName === "INPUT"){
+        // Todo: Update the task status in the back-end
+
+        const taskId = e.target.closest('li').id.substring(5);
+        const task = {
+            description: e.target.nextElementSibling.innerText,
+            status: e.target.checked,
+            email: loggedUser.email
+        };
+
+        fetch(`${API_URL}/tasks/${taskId}`, {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(task)
+        }).then(res => {
+            if (!res.ok){
+                e.target.checked = false;
+                alert("Failed to update the task status");
+            }
+        }).catch(err => {
+            e.target.checked = false;
+            alert("Something went wrong, try again");
+        })
+    }
+});
+
